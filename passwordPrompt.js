3acc172456e4f07375edb2aba8b7de9ba5a480d7
Camelle <baloiscamelle@gmail.com>
2024-10-09 18:43:38 +0800

    git commit -m "initialcommit"

---
+++ passwordPrompt.js
@@ -0,0 +1,11 @@
+let correctPassword = "camyang25";
+let userPassword;
+
+do {
+    userPassword = prompt("Please enter your password:");
+    if (userPassword !== correctPassword) {
+        alert("Invalid password, try again");
+    }
+} while (userPassword !== correctPassword);
+
+alert("Welcome! You've entered the correct password.")
